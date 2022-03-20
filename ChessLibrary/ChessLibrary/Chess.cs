234ff9d7a9b21afd47036c349336a7718ec66663
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-03-20 23:58:05 +0700

    Development Update#1
    - imlemented class Board;
    - the main class was extended.

--- ChessLibrary/ChessLibrary/Chess.cs
+++ ChessLibrary/ChessLibrary/Chess.cs
@@ -8,21 +8,32 @@ namespace ChessLibrary
 {
     public class Chess
     {
-        public string fen;
+        public string fen { get; private set; }
+        Board board;
+
         public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")//starting position for Chess
         {
             this.fen = fen;
+            board = new Board(fen);
+        }
+
+        Chess (Board board)
+        {
+            this.board = board;
         }
 
         public Chess Move(string move)
         {
-            Chess nextChess = new Chess(fen);
+            FigureMoving fm = new FigureMoving(move);
+            Board nextBoard = board.Move(fm);
+            Chess nextChess = new Chess(nextBoard);
             return nextChess;
         }
 
         public char GetFigureAt(int x, int y)//that method show where are all figures and pawns right now
         {
-            return '.';
+            Position position = new Position();
+
         }
     }
 }
